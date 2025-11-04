b012fb71c56e51a2c388f3c9c7834f7be8ef3ec7
najmus-sakib-hossain <manfromexistence@proton.me>
2025-11-04 10:58:54 +0600

    feat: Update Navbar component to use lucide-react icons and improve link structure

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,11 +1,9 @@
+import { Menu, MenuIcon, Settings2, X } from "lucide-react";
 import { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import { IoClose } from "react-icons/io5";
-// import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const location = useLocation();
+  const location = { pathname: window.location.pathname };
 
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -13,190 +11,185 @@ const Navbar = () => {
 
   const getTextColor = () => {
     if (location.pathname === "/") return "text-slate-950";
-    if (location.pathname === "/main/about-me") return "text-white";
-    if (location.pathname === "/main/life-event") return "text-white";
-    if (location.pathname === "/resource/entepreneourship") return "text-white";
-    if (location.pathname === "/resource/technology") return "text-white";
-    if (location.pathname === "/resource/donation") return "text-white";
+    if (location.pathname === "/aboutus") return "text-white";
+    if (location.pathname === "/life-events") return "text-white";
+    if (location.pathname === "/entrepreneurship") return "text-white";
+    if (location.pathname === "/technology") return "text-white";
+    if (location.pathname === "/donation") return "text-white";
     if (location.pathname === "/contact") return "text-white";
-    if (location.pathname === "/ai-summit") return "text-white";
     return "text-black";
   };
 
   const getIconColor = () => {
     if (location.pathname === "/") return "text-slate-950";
-    if (location.pathname === "/main/about-me") return "text-white";
-    if (location.pathname === "/main/life-event") return "text-white";
-    if (location.pathname === "/resource/entepreneourship") return "text-white";
-    if (location.pathname === "/resource/technology") return "text-white";
-    if (location.pathname === "/resource/donation") return "text-slate-900";
+    if (location.pathname === "/aboutus") return "text-white";
+    if (location.pathname === "/life-events") return "text-white";
+    if (location.pathname === "/entrepreneurship") return "text-white";
+    if (location.pathname === "/technology") return "text-white";
+    if (location.pathname === "/donation") return "text-slate-900";
     if (location.pathname === "/contact") return "text-white";
-    if (location.pathname === "/ai-summit") return "text-white";
     return "text-black";
   };
 
-  const getBgColor = () => {
-    if (location.pathname === "/ai-summit") return "bg-[#2E5AFF]";
-  };
-
   return (
-    <div className={` ${getBgColor()}`}>
+    <div className={``}>
       <div className={`py-6 w-11/12 mx-auto`}>
         <div className="flex items-center justify-between">
-          {/* <div className=" text-4xl">Shahriar Khan</div> */}
-          <Link to="/">
+          <a href="/">
             <div className={`text-4xl special-text ${getTextColor()}`}>
               Shahriar Khan
             </div>
-          </Link>
+          </a>
           <div>
             <p
               className="text-4xl cursor-pointer px-4 py-2 rounded-lg"
               onClick={toggleDrawer}
             >
-              <FaBars size={36} className={`text-2xl ${getIconColor()}`} />
+              {/* <FaBars size={36} className={`text-2xl ${getIconColor()}`} /> */}
+              <MenuIcon className="h-8 w-8 text-[#f00]" />
+              {/* Navbar */}
             </p>
           </div>
         </div>
 
         {/* Drawer */}
         <div
-          className={`fixed top-0 right-0 w-3/4 lg:w-2/5 h-full bg-white text-slate-900 p-4 transform transition-transform ${
-            drawerOpen ? "translate-x-0" : "translate-x-full"
-          } z-50`}
+          className={`fixed top-0 right-0 w-3/4 lg:w-2/5 h-full bg-white text-slate-900 p-4 transform transition-transform ${drawerOpen ? "translate-x-0" : "translate-x-full"
+            } z-50`}
         >
           <div className="flex justify-end">
             <button
               className="text-4xl text-slate-950 cursor-pointer"
               onClick={toggleDrawer}
             >
-              <IoClose />
+              {/* <IoClose /> */}
+              <X className="h-8 w-8 text-[#f00]" />
+              {/* Close */}
             </button>
           </div>
           <div className="mt-10 text-3xl font-medium px-20">
             <ul>
-              <NavLink
-                to="/"
+              <a
+                href="/"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Home</li>
-              </NavLink>
-              <NavLink
-                to="/main/about-me"
+              </a>
+              <a
+                href="/aboutus"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/aboutus"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">About Me</li>
-              </NavLink>
-              {/* <li className="mb-6">Publications</li> */}
-              <NavLink
-                to="/resource/books"
+              </a>
+              <a
+                href="/resource/books"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/books"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Books</li>
-              </NavLink>
-              <NavLink
-                to="/resource/entepreneourship"
+              </a>
+              <a
+                href="/resource/entepreneourship"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/entepreneourship"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Entepreneourship</li>
-              </NavLink>
-              <NavLink
-                to="/resource/events"
+              </a>
+              <a
+                href="/resource/events"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/events"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Events</li>
-              </NavLink>
-              <NavLink
-                to="/resource/blogs"
+              </a>
+              <a
+                href="/resource/blogs"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/blogs"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Blogs</li>
-              </NavLink>
-              <NavLink
-                to="/resource/technology"
+              </a>
+              <a
+                href="/resource/technology"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/technology"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Technology</li>
-              </NavLink>
-              <NavLink
-                to="/resource/donation"
+              </a>
+              <a
+                href="/resource/donation"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/donation"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Donation</li>
-              </NavLink>
-              <NavLink
-                to="/resource/videos"
+              </a>
+              <a
+                href="/resource/videos"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/resource/videos"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Videos</li>
-              </NavLink>
-              <NavLink
-                to="/main/life-event"
+              </a>
+              <a
+                href="/main/life-event"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/main/life-event"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Life Events</li>
-              </NavLink>
-              <NavLink
-                to="/contact"
+              </a>
+              <a
+                href="/contact"
                 onClick={toggleDrawer}
-                className={({ isActive }) =>
-                  isActive
+                className={
+                  location.pathname === "/contact"
                     ? "text-blue-500 font-bold"
                     : "text-black hover:text-blue-500"
                 }
               >
                 <li className="mb-6">Contact</li>
-              </NavLink>
+              </a>
             </ul>
           </div>
         </div>
