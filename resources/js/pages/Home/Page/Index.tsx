2378af80c118d01f6dc4a4018c90bb52a340d37f
najmus-sakib-hossain <manfromexistence@proton.me>
2025-11-04 13:18:33 +0600

    refactor: simplify AnimatedGridPattern component by removing unused props and animations

--- resources/js/pages/Home/Page/Index.tsx
+++ resources/js/pages/Home/Page/Index.tsx
@@ -4,16 +4,12 @@ import { cn } from "@/lib/utils";
 
 const Home = () => {
     return (
-        <div className="bg-background relative flex h-[500px] w-full items-center justify-center overflow-hidden rounded-lg border p-20">
+        <div className="bg-background relative flex min-h-screen w-full items-center justify-center overflow-hidden">
             <AnimatedGridPattern
                 numSquares={30}
                 maxOpacity={0.1}
                 duration={3}
                 repeatDelay={1}
-                className={cn(
-                    "mask-[radial-gradient(500px_circle_at_center,white,transparent)]",
-                    "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
-                )}
             />
         </div>
         // <div className="relative flex-col bg-black min-h-screen text-[#272121] flex items-center justify-center overflow-hidden">
