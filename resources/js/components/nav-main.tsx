aca3b266618bf1ae4d9c0d2418849220c86529d3
najmus-sakib-hossain <manfromexistence@proton.me>
2025-11-04 17:32:08 +0600

    refactor: comment out unused NavProjects and SidebarGroupLabel components

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -32,7 +32,7 @@ export function NavMain({
 }) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Admin Panel</SidebarGroupLabel>
+      {/* <SidebarGroupLabel>Admin Panel</SidebarGroupLabel> */}
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible
