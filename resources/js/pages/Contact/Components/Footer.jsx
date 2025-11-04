fdabe55c2a9624c1d2a267c6867e5c688ce3027c
najmus-sakib-hossain <manfromexistence@proton.me>
2025-11-04 23:31:35 +0600

    fix: Correct footer height and fix typo in Pinterest label

--- resources/js/pages/Contact/Components/Footer.jsx
+++ resources/js/pages/Contact/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-[#2E5AFF] py-6">
-      <div className="flex items-center justify-center gap-6 flex-wrap lg:flex-nowrap text-white font-semibold text-xl">
+    <div className="bg-[#2E5AFF] h-[135px]">
+      <div className="h-full flex items-center justify-center gap-6 flex-wrap lg:flex-nowrap text-white font-semibold text-xl">
         <a href="https://www.linkedin.com/in/mohammadshahriarkhan/">
           {" "}
           <h1>LINKEDIN</h1>
@@ -13,7 +13,7 @@ const Footer = () => {
           <h1>FACEBOOK</h1>
         </a>
         <h1>INSTAGRAM</h1>
-        <h1>PINTAREST</h1>
+        <h1>PINTEREST</h1>
       </div>
     </div>
   );
--- resources/js/pages/Contact/Page/Contact.jsx
+++ resources/js/pages/Contact/Page/Contact.jsx
@@ -4,7 +4,7 @@ import Footer from "../Components/Footer";
 
 const Contact = () => {
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex flex-col h-screen">
       <div className="w-full absolute top-0 left-0 z-50 bg-transparent">
         <Navbar />
       </div>{" "}
